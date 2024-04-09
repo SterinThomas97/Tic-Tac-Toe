7c8ec3a165d415746eb85fa213b8f92331a9ee22
Sterin Thomas <Sterin.THomas@griffithuni.edu.au>
2024-04-09 14:14:21 +1000

    Design the two new pages: Rules and Credits

--- Tic-Tac/screens/Credits.js
+++ Tic-Tac/screens/Credits.js
@@ -1,9 +1,63 @@
-import { Text } from "react-native";
+
+import { Text, StyleSheet, View, ScrollView } from "react-native";
+import TitleButton from "../components/TitleButton";
+import { useNavigation } from "@react-navigation/native";
+import AppBottomButton from "../components/AppBottomButton";
+import { credits } from "../components/Message";
 
 function Credits() {
-    return (
-        <Text>Credits screen</Text>
+    const navigation = useNavigation();
+    const creditMessage = credits;
+
+    function backButtonHandler() {
+        navigation.navigate('Home');
+    }
+
+    return(
+        <View style={styles.appContainer}>
+            <View >
+                <TitleButton title="Credits"/>
+            </View>
+            <ScrollView style={styles.messageContainer}>
+                <Text style={{color: 'white', padding: 10, fontSize: 14}}>{creditMessage}</Text>
+            </ScrollView>
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <AppBottomButton title="Back" onPress={backButtonHandler}/>
+                </View>
+            </View>
+        </View>
     )
 }
 
 export default Credits;
+
+const styles = StyleSheet.create({
+    appContainer: {
+        flex: 1,
+        alignItems : 'center',
+        justifyContent: 'center',
+        backgroundColor: '#c6c9c5',
+        marginTop: 50,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 50
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end'
+    },
+    button: {
+        backgroundColor: 'blue',
+        margin : 10,
+        borderRadius: 20
+    },
+    messageContainer: {
+        backgroundColor: "#373736",
+        height: 400,
+        marginHorizontal: 40,
+        marginTop: 30
+
+    }
+});
--- Tic-Tac/screens/Rules.js
+++ Tic-Tac/screens/Rules.js
@@ -1,10 +1,62 @@
 
-import { Text } from "react-native";
+import { Text, StyleSheet, View, ScrollView } from "react-native";
+import TitleButton from "../components/TitleButton";
+import { useNavigation } from "@react-navigation/native";
+import AppBottomButton from "../components/AppBottomButton";
+import { rules } from "../components/Message";
 
 function Rules() {
-    return (
-        <Text>Rules screen</Text>
+    const navigation = useNavigation();
+
+    function backButtonHandler() {
+        navigation.navigate('Home');
+    }
+    const rulesMessage = rules;
+    return(
+        <View style={styles.appContainer}>
+            <View>
+                <TitleButton title="Rules"/>
+            </View>
+            <ScrollView style={styles.messageContainer}>
+                <Text style={{color: 'white', padding: 10, fontSize: 14}}>{rules}</Text>
+            </ScrollView>
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <AppBottomButton title="Back" onPress={backButtonHandler}/>
+                </View>
+            </View>
+        </View>
     )
 }
 
 export default Rules;
+
+const styles = StyleSheet.create({
+    appContainer: {
+        flex: 1,
+        alignItems : 'center',
+        justifyContent: 'center',
+        backgroundColor: '#c6c9c5',
+        marginTop: 50,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 50
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-end'
+    },
+    button: {
+        backgroundColor: 'blue',
+        margin : 30,
+        borderRadius: 20
+    },
+    messageContainer: {
+        backgroundColor: "#373736",
+        height: 400,
+        marginHorizontal: 40,
+        marginTop: 30
+
+    }
+});
